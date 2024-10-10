fac32709d7c88d2b0dd9cc4eb71ade123afec105
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-10-10 21:09:45 +0900

    errorElement

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const routes = [
   {
@@ -11,12 +13,14 @@ const routes = [
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ];
 const router = createBrowserRouter(routes);
