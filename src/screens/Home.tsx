b573bd3223e51c8ab6e12273cf876860d830df49
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-10-11 20:07:27 +0900

    Extras

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  // console.log(readSearchParams.has("geo"));
+  // console.log(readSearchParams.get("geo"));
+  // setTimeout(() => {
+  //   console.log("url search params set...");
+  //   setSearchParams({
+  //     day: "today",
+  //     tommorow: "124",
+  //   });
+  // }, 3000);
   return (
     <>
       <h1>Users</h1>
